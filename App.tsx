c31a5f92dccc1f7644fe5b72d1c92b240d5d333b
saman <saman.arif@veeva.com>
2021-09-28 17:46:29 -0400

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useLogging } from './src/hooks/useLogging';
+
+export default function App() {
+    const [logging] = useLogging('Application');
+
+    useEffect(() => {
+        logging.info('Loading application.');
+    }, [logging])
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: '1',
+        borderStyle: 'dotted',
+        borderColor: 'red'
+    },
+});
